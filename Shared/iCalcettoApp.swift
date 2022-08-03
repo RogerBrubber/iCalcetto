f664f1f0b851903a17789fad7e101b9b9f6c15c8
brubber <alessandro.ruberto23@gmail.com>
2022-08-03 17:13:31 +0200

    First commit

---
+++ Shared/iCalcettoApp.swift
@@ -0,0 +1,17 @@
+//
+//  iCalcettoApp.swift
+//  Shared
+//
+//  Created by Alessandro Ruberto on 03/08/22.
+//
+
+import SwiftUI
+
+@main
+struct iCalcettoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
